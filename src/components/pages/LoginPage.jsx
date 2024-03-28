e8863784f1a822def1a6e54f3700ac74e4477877
lgmb10 <louisgambier10@gmail.com>
2024-03-28 12:35:16 +0100

    :sparkles: F01 > Add title and form

--- src/components/pages/LoginPage.jsx
+++ src/components/pages/LoginPage.jsx
@@ -2,10 +2,9 @@ import {useState} from 'react';
 
 export default function LoginPage() {
     const [firstName, setFirstName] = useState("")
-    //let validation = useRef(true)
 
 
-    const handleClick = (event) => {
+    const handleSubmit = (event) => {
         event.preventDefault();
         alert("Bonjour " + firstName);
         setFirstName("");
@@ -16,11 +15,11 @@ export default function LoginPage() {
             <div>
                 <h1>Bienvenue chez nous !</h1>
                 <h2>Connectez vous</h2>
-                <form action={"submit"}>
-                    <input id={"firstNameInput"} required type="text" placeholder={"Entrez votre prénom"}
+                <form action={"submit"} onSubmit={handleSubmit}>
+                    <input required type="text" placeholder={"Entrez votre prénom"}
                            value={firstName}
                            onChange={(event) => setFirstName(event.target.value)}/>
-                    <button type={"submit"} onClick={(event) => handleClick(event)}>Accéder à votre espace</button>
+                    <button type={"submit"}>Accéder à votre espace</button>
                 </form>
             </div>
         </>
