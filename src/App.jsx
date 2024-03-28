17e2cd3068f8e4398fd1c95c744287d3ef36af30
lgmb10 <louisgambier10@gmail.com>
2024-03-28 12:47:40 +0100

    :art: F01 : LOGIN > create login form

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,5 @@
-import {useRef, useState} from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-import LoginPage from "./components/pages/LoginPage.jsx";
+import LoginPage from "./components/pages/login/LoginPage.jsx";
 
 function App() {
 
