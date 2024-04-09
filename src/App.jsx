1706a63524978f6452bbccacba614a49d5db02ad
lgmb10 <louisgambier10@gmail.com>
2024-04-09 15:21:11 +0200

    :art: F02 : create orderPage and ErrorPage and set redirection

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
 import './App.css'
 import LoginPage from "./components/pages/login/LoginPage.jsx";
+import {Route, Routes} from "react-router-dom";
+import ErrorPage from "./components/pages/error/ErrorPage.jsx";
+import OrderPage from "./components/pages/order/OrderPage.jsx";
+import React from "react";
 
 function App() {
 
     return (
-        <>
-            <LoginPage />
-        </>
+        <Routes>
+            <Route path={"/"} element={<LoginPage/>} />
+            <Route path={"/order/:userId"} element={<OrderPage/>} />
+            <Route path={"*"} element={<ErrorPage/>} />
+        </Routes>
     )
 }
 
